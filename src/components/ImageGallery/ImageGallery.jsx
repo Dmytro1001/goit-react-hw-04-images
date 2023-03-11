6fdf30a8d72846970c863771080ba0c9403adf60
Dmytro1001 <31dmitri13@gmail.com>
2023-03-11 22:23:38 +0200

    test

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ToastContainer, toast } from 'react-toastify';
 import { fetchImages } from '../../services/getFetchImages';
@@ -7,93 +7,83 @@ import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Button } from 'components/Button/Button';
 import { Loader } from 'components/Loader/Loader';
 
-export class ImageGallery extends Component {
-  state = {
-    request: '',
-    images: [],
-    totalHits: null,
-    page: 1,
-    loadBtnShown: false,
-    error: null,
-  };
+export const ImageGallery = ({ value, onClick }) => {
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [, setError] = useState(null);
+  const [loadBtnShown, setLoadBtnShown] = useState(true);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevProps.value;
-    const currentQuery = this.props.value;
-    const prevPage = prevState.page;
-    const currentPage = this.state.page;
-    const prevStateRequest = prevState.request;
-
-    if (prevQuery !== currentQuery) {
-      this.setState(prev => ({
-        ...prev,
-        images: [],
-        page: 1,
-        request: currentQuery,
-        loadBtnShown: true,
-      }));
+  useEffect(() => {
+    if (value !== '') {
+      getImages(value, 1);
     }
 
-    if (
-      prevStateRequest !== this.state.request ||
-      (prevPage !== currentPage && currentPage !== 1)
-    ) {
-      this.setState({ loading: true, page: currentPage });
+    return () => {
+      setImages([]);
+      setPage(1);
+    };
+  }, [value]);
 
-      fetchImages(currentQuery, currentPage)
-        .then(data => {
-          if (data.total === 0) {
-            this.setState({ loading: false });
-            return toast.warn(`Nothing was found for ${currentQuery}`, {
-              position: 'top-center',
-              theme: 'colored',
-            });
-          }
-
-          if (data.hits.length < 12) {
-            this.setState({ loadBtnShown: false });
-          }
-
-          this.setState(state => ({
-            images: [...state.images, ...data.hits],
-            loading: false,
-          }));
-        })
-        .catch(error => this.setState({ error }));
+  useEffect(() => {
+    if (page > 1) {
+      getImages(value, page);
     }
-  }
+  }, [page]);
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1, loading: true }));
+  const getImages = async (value, page) => {
+    await fetchImages(value, page)
+      .then(data => {
+        if (data.total === 0) {
+          setLoading(false);
+          toast.error(`Nothing was found for ${value}`, {
+            position: 'top-center',
+            theme: 'colored',
+          });
+
+          return;
+        }
+
+        if (data.hits.length < 12) {
+          setLoadBtnShown(false);
+        }
+
+        setImages(images => [...images, ...data.hits]);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+      });
   };
 
-  render() {
-    const { images, loading, loadBtnShown } = this.state;
+  const handleLoadMore = () => {
+    setLoading(true);
+    setPage(page => page + 1);
+  };
 
-    return (
-      <>
-        <ImageGalleryList>
-          {images.length > 0 &&
-            images.map(item => {
-              return (
-                <ImageGalleryItem
-                  key={item.id}
-                  url={item.webformatURL}
-                  onClick={this.props.onClick}
-                  bigImage={item.largeImageURL}
-                />
-              );
-            })}
-        </ImageGalleryList>
-        {images.length > 0 && !loading && loadBtnShown && (
-          <Button onClick={this.handleLoadMore} />
-        )}
-        {loading && <Loader />}
-        <ToastContainer autoClose={1500} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <ImageGalleryList>
+        {images.length > 0 &&
+          images.map(item => {
+            return (
+              <ImageGalleryItem
+                key={item.id}
+                url={item.webformatURL}
+                onClick={onClick}
+                bigImage={item.largeImageURL}
+              />
+            );
+          })}
+      </ImageGalleryList>
+      {images.length > 0 && !loading && loadBtnShown && (
+        <Button onClick={handleLoadMore} />
+      )}
+      {loading && <Loader />}
+      <ToastContainer autoClose={3000} />
+    </>
+  );
+};
 
 ImageGallery.propTypes = {
   value: PropTypes.string.isRequired,
