1c2249e7a6eeb4634a7367f199631932c976f965
Dmytro1001 <31dmitri13@gmail.com>
2023-03-12 00:24:38 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Modal } from './Modal/Modal';
 import { HeaderSearchbar } from './Searchbar/Searchbar';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { fetchImages } from '../services/getFetchImages';
 import { Container } from './App.styles';
 import { GlobalStyles } from './GlobalStyle';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
 
 export const App = () => {
-  const [textSearch, setTextSearch] = useState('');
+  const [textSearch, setTextSearch] = useState(null);
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [, setError] = useState(null);
+  const [loadBtnShown, setLoadBtnShown] = useState(true);
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [currentImage, setCurrentImage] = useState(null);
 
@@ -23,12 +31,54 @@ export const App = () => {
 
   const handleSubmit = textSearch => {
     setTextSearch(textSearch);
+    setPage(1);
+    setImages([]);
+  };
+
+  useEffect(() => {
+    if (textSearch === null) {
+      return;
+    }
+    getImages(textSearch, page);
+  }, [textSearch, page]);
+
+  const getImages = async (value, page) => {
+    await fetchImages(value, page)
+      .then(data => {
+        if (data.total === 0) {
+          setLoading(false);
+          toast.error(`Nothing was found for ${value}`, {
+            position: 'top-center',
+            theme: 'colored',
+          });
+        }
+
+        if (data.hits.length < 12) {
+          setLoadBtnShown(false);
+        }
+
+        setImages(images => [...images, ...data.hits]);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+      });
+  };
+
+  const handleLoadMore = () => {
+    setLoading(true);
+    setPage(page => page + 1);
   };
 
   return (
     <Container>
       <HeaderSearchbar onSubmit={handleSubmit} />
-      <ImageGallery value={textSearch} onClick={openModal} />
+      {images.length > 0 && <ImageGallery value={images} onClick={openModal} />}
+
+      {images.length > 0 && !loading && loadBtnShown && (
+        <Button onClick={handleLoadMore} />
+      )}
+      {loading && <Loader />}
       {isOpenModal && (
         <Modal onClose={toggleModal} currentImage={currentImage} />
       )}
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,91 +1,27 @@
-import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { ToastContainer, toast } from 'react-toastify';
-import { fetchImages } from '../../services/getFetchImages';
 import { ImageGalleryList } from './ImageGallery.styles';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Button } from 'components/Button/Button';
-import { Loader } from 'components/Loader/Loader';
 
 export const ImageGallery = ({ value, onClick }) => {
-  const [page, setPage] = useState(1);
-  const [images, setImages] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [, setError] = useState(null);
-  const [loadBtnShown, setLoadBtnShown] = useState(true);
-
-  useEffect(() => {
-    if (value !== '') {
-      getImages(value, 1);
-    }
-
-    return () => {
-      setImages([]);
-      setPage(1);
-    };
-  }, [value]);
-
-  useEffect(() => {
-    if (page > 1) {
-      getImages(value, page);
-    }
-  }, [page]);
-
-  const getImages = async (value, page) => {
-    await fetchImages(value, page)
-      .then(data => {
-        if (data.total === 0) {
-          setLoading(false);
-          toast.error(`Nothing was found for ${value}`, {
-            position: 'top-center',
-            theme: 'colored',
-          });
-
-          return;
-        }
-
-        if (data.hits.length < 12) {
-          setLoadBtnShown(false);
-        }
-
-        setImages(images => [...images, ...data.hits]);
-        setLoading(false);
-      })
-      .catch(error => {
-        setError(error);
-      });
-  };
-
-  const handleLoadMore = () => {
-    setLoading(true);
-    setPage(page => page + 1);
-  };
-
   return (
     <>
       <ImageGalleryList>
-        {images.length > 0 &&
-          images.map(item => {
-            return (
-              <ImageGalleryItem
-                key={item.id}
-                url={item.webformatURL}
-                onClick={onClick}
-                bigImage={item.largeImageURL}
-              />
-            );
-          })}
+        {value.map(item => {
+          return (
+            <ImageGalleryItem
+              key={item.id}
+              url={item.webformatURL}
+              onClick={onClick}
+              bigImage={item.largeImageURL}
+            />
+          );
+        })}
       </ImageGalleryList>
-      {images.length > 0 && !loading && loadBtnShown && (
-        <Button onClick={handleLoadMore} />
-      )}
-      {loading && <Loader />}
-      <ToastContainer autoClose={3000} />
     </>
   );
 };
 
 ImageGallery.propTypes = {
-  value: PropTypes.string.isRequired,
+  value: PropTypes.array.isRequired,
   onClick: PropTypes.func.isRequired,
 };
